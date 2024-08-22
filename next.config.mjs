756f41f713c97174788c34dcee8a5cc5d4cd403b
jaewoong2 <jwisgenius@naver.com>
2024-08-22 14:18:48 +0900

    :sparkles: Cloudfront로 Static 파일 정적 서빙 CI/CD 및 옵션 값 설정  (#3)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  reactStrictMode: true,
+  assetPrefix:
+    process.env.NODE_ENV === 'production'
+      ? 'https://noting.prlc.kr'
+      : undefined,
   images: {
     remotePatterns: [
       { hostname: 'lh3.googleusercontent.com' },
